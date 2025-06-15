cb29b0c4adef90c14a3d1aca8b1786340e1e2afd
Atokolo John <atokoloj@gmail.com>
2025-06-15 12:16:28 +0100

    updated file

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   // Select DOM elements
-  const addButton = document.getElementById('add-task');
+  const add-task-btn = document.getElementById('add-task');
   const taskInput = document.getElementById('task-input');
   const taskList = document.getElementById('task-list');
 
